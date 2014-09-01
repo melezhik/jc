d6b181cf5a7588f926f97006247c303d1c3d5db3
Alexey Melezhik <melezhik@gmail.com>
2014-09-01 13:36:04 +0400

    set install base

--- jc/app/controllers/builds_controller.rb
+++ jc/app/controllers/builds_controller.rb
@@ -1,4 +1,5 @@
 require 'fileutils'
+
 class BuildsController < ApplicationController
 
     def index 
@@ -17,9 +18,9 @@ class BuildsController < ApplicationController
     
         @build.save!
 
-
         FileUtils.mkdir_p "#{Dir.home}/.jc/builds/#{@build.id}"
-        render :text => "build ID: #{@build.id}. key:#{_params[:key_id]} created ok\n"
+
+        render :text => "created build ID: #{@build.id}. key:#{_params[:key_id]} ok\n"
 
     end
 
@@ -28,17 +29,40 @@ class BuildsController < ApplicationController
         @build = Build.find params[:id]
         dir_name = params[:path].sub('.tar.gz','')
 
-        cmd = "cd #{Dir.home}/.jc/builds/#{@build.id} && rm -rf *"
-        cmd << " && cp  -v #{Dir.home}/.jc/artefacts/#{params[:path]} ."
-        cmd << " && tar -xzf #{params[:path]} && cd #{dir_name} && cp -r cpanlib/ ../ "
-        cmd << " && cd ../ && rm -rf #{dir_name} && rm #{params[:path]} && ls -l cpanlib/"
-        out = `#{cmd}`
+        File.open("#{Dir.home}/.jc/builds/#{@build.id}/log.txt", 'a') do |l|
+            l << "set install base from: #{params[:path]}\n\n"
+        end
 
-        render :text => "ustall base from: #{params[:path]} set ok\n output: #{out}"
+        cmd = [ ]
+        cmd << "cd #{Dir.home}/.jc/builds/#{@build.id}"
+        cmd << "rm -rf #{dir_name}"
+        cmd << "rm -rf cpanlib"
+        cmd << "cp  -v #{Dir.home}/.jc/artefacts/#{params[:path]} ."
+        cmd << "tar -xzf #{params[:path]}"
+        cmd << "cd #{dir_name}"
+        cmd << "cp -r cpanlib/ ../"
+        cmd << "cd ../"
+        cmd << "rm -rf #{dir_name} #{params[:path]}"
+
+        cmd_str = cmd.map { |c| "#{c} 1>>#{Dir.home}/.jc/builds/#{@build.id}/log.txt 2>&1" }.join ' && '
+
+        File.open("#{Dir.home}/.jc/builds/#{@build.id}/log.txt", 'a') do |l|
+            l << "running command: #{cmd_str}\n\n"
+        end
+
+        if system(cmd_str) == true
+            File.open("#{Dir.home}/.jc/builds/#{@build.id}/log.txt", 'a') do |l|
+                l << "set install base ok\n"
+            end
+            render :text => "set install base from: #{params[:path]} ok\n"
+        else
+            render :text => "command #{cmd_str} failed\n", :status => 500
+        end
 
     end
 
 private
+
 
     def _params
         params.require(:build).permit( 
