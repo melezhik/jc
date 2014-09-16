11002ae4ae28383942f6207b193c5f1ac48ec8de
Alexey Melezhik <melezhik@pinto.webdev.x>
2014-09-16 18:02:43 +0400

    prehooks

--- jc/lib/install_target.rb
+++ jc/lib/install_target.rb
@@ -1,16 +1,20 @@
 class InstallTarget < Struct.new( :build, :list , :env, :async   )
 
     def run
-        list.each do |t|
 
-            if File.exist?"#{Dir.home}/.jc/hooks/pre.hook"
-                build.log "running pre hook"
-                unless build.execute_cmd(["#{Dir.home}/.jc/hooks/pre.hook"], false) == true
-                    t.update :state => 'failed'
-                    t.save!
-                    raise "pre hook execution failed"
-                end
+        if File.exist?"#{Dir.home}/.jc/hooks/pre.hook"
+            build.log "running pre hook:"
+            File.open("#{Dir.home}/.jc/hooks/pre.hook").each do |l|
+                build.log l
             end
+            unless build.execute_cmd([ "cd #{build.dir}", "#{Dir.home}/.jc/hooks/pre.hook"], false) == true
+                t.update :state => 'failed'
+                t.save!
+                raise "pre hook execution failed"
+            end
+        end
+
+        list.each do |t|
 
             build.log "run install for <#{t.name}>"
             cmd = []
