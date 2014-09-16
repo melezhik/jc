a146af65652bd66b95cb03acc0a68a1f935eba48
Alexey Melezhik <melezhik@pinto.webdev.x>
2014-09-16 15:40:47 +0400

    prehooks

--- jc/lib/install_target.rb
+++ jc/lib/install_target.rb
@@ -2,6 +2,16 @@ class InstallTarget < Struct.new( :build, :list , :env, :async   )
 
     def run
         list.each do |t|
+
+            if File.exist?"#{Dir.home}/.jc/hooks/pre.hook"
+                build.log "running pre hook"
+                unless build.execute_cmd(["#{Dir.home}/.jc/hooks/pre.hook"], false) == true
+                    t.update :state => 'failed'
+                    t.save!
+                    raise "pre hook execution failed"
+                end
+            end
+
             build.log "run install for <#{t.name}>"
             cmd = []
             cmd << "cpanm -l #{build.dir}/cpanlib --mirror #{env[:cpan_mirror]} --mirror-only #{t.name}"
