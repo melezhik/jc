1e9a26661e84cde396991be81f2598e66f7e9704
Alexey Melezhik <melezhik@pinto.webdev.x>
2014-09-26 14:00:15 +0400

    key_id not unqiue

--- jc/db/migrate/20140829124953_create_builds.rb
+++ jc/db/migrate/20140829124953_create_builds.rb
@@ -2,9 +2,7 @@ class CreateBuilds < ActiveRecord::Migration
   def change
     create_table :builds do |t|
       t.integer :key_id
-
       t.timestamps
     end
-    add_index :builds, :key_id
   end
 end
